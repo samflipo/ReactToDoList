b3ce758b339a24878c8cfe241257d7516275c16b
Samuel Habtemariam <samuel.habt@gmail.com>
2019-02-22 11:03:04 -0800

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,67 +3,6 @@ import PropTypes from 'prop-types';
 
 import './Todo.css';
 
-// create an app with a todo list
-// export default class Todo extends Component {
-//   state = {
-//     tasks: [
-//       'task one',
-//       'task two',
-//       'task three',
-//       'task four'
-//     ]
-//   }
-//
-//   handleSubmit = (e) => {
-//     e.preventDefault()
-//     this.setState({
-//       tasks: [...this.state.tasks, e.target[0].value]
-//     })
-//     e.target[0].value = ''
-//   }
-//
-//   removeTask = (task) => {
-//     this.setState({
-//       tasks: this.state.tasks.filter(item => item !== task)
-//     })
-//   }
-//
-//   removeAll = () => {
-//     this.setState({ tasks: []})
-//   }
-//
-//   render () {
-//    return (
-//      <div className='App'>
-//         <h1>Todo list</h1>
-//         <div className='results'>
-//             <ol>
-//               { !this.state.tasks.length
-//                   ? <span>No tasks found</span>
-//
-//                   : this.state.tasks.map((task, i) => <TaskItem
-//                       removeTask={this.removeTask}
-//                       key={i}
-//                       item={task}/>)
-//               }
-//             </ol>
-//         </div>
-//         <div className='action-container'>
-//           <div className='add'>
-//             <form onSubmit={this.handleSubmit}>
-//               <input type='text' />
-//               <button>Add task</button>
-//             </form>
-//           </div>
-//           <div className='remove'>
-//             <button onClick={this.removeAll}>Remove all tasks</button>
-//           </div>
-//         </div>
-//      </div>
-//    )
-//  }
-// }
-
 export default class Todo extends Component {
   handleSubmit = (e) => {
     e.preventDefault()
